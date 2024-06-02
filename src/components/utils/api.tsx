05197d1b92baee8f0f48b9475dc37df6f2451331
Bruno Matheus Pereira Silva <82674117+bruno9800@users.noreply.github.com>
2024-06-02 00:21:12 -0300

    corrigindo bg-color e erro api gemini

--- src/components/utils/api.tsx
+++ src/components/utils/api.tsx
@@ -1,16 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI('AIzaSyC6Y0qOQJrLWPrQBim7oKE7nnJ0l-k00b4');
-  
-async function useGeminiApiQuestion() {
-  try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    const qt_perguntas = 10;
-    const idioma_fonte = "italiano";
-    const idioma_objeto = "inglês";
-    const json_schema =
-      ' Questão = {"question: any" : str, "reposta" : str, "alternatives" : str} ';
-    const exemplos = `[
+const genAI = new GoogleGenerativeAI("AIzaSyC6Y0qOQJrLWPrQBim7oKE7nnJ0l-k00b4");
+
+export async function UseGeminiApiQuestion() {
+	try {
+		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+		const qt_perguntas = 10;
+		const idioma_fonte = "italiano";
+		const idioma_objeto = "inglês";
+		const json_schema =
+			' Questão = {"question: any" : str, "reposta" : str, "alternatives" : str} ';
+		const exemplos = `[
       {
         "question": "Como você traduziria a palavra \\"pato\\" para o inglês?",
         "answer": "duck",
@@ -37,36 +37,37 @@ async function useGeminiApiQuestion() {
         "alternatives": ["Livro", "Mesa", "Caderno", "Folha"]
       }
     ]`; // Exemplos de perguntas no formato JSON
-    const prompt =
-      "Me dê " +
-      qt_perguntas +
-      " perguntas aleatórias em formato de quiz a respeito da tradução de palavras " +
-      "quaisquer no idioma fonte: " +
-      idioma_fonte +
-      " para o idioma objeto: " +
-      idioma_objeto +
-      ". Usando o seguinte esquema JSON: " +
-      json_schema +
-      " Retorne apenas list[Questão]" +
-      ". Seguindo os exemplos: " +
-      exemplos +
-      ". Deixe todas as palavras em formato UNICODE";
+		const prompt =
+			"Me dê " +
+			qt_perguntas +
+			" perguntas aleatórias em formato de quiz a respeito da tradução de palavras " +
+			"quaisquer no idioma fonte: " +
+			idioma_fonte +
+			" para o idioma objeto: " +
+			idioma_objeto +
+			". Usando o seguinte esquema JSON: " +
+			json_schema +
+			" Retorne apenas list[Questão]" +
+			". Seguindo os exemplos: " +
+			exemplos +
+			". Deixe todas as palavras em formato UNICODE";
 
-    const result = await model.generateContent(prompt);
+		const result = await model.generateContent(prompt);
 
-    const response = await result.response;
+		const response = result.response;
 
-    if (response) {
-      const text = response.text().replace("```json\n", '').replace("\n```", '');
-      // console.log(text);
-      return text;
-    } else {
-      throw new Error("Empty response received from Google Generative AI");
-    }
-  } catch (error) {
-    console.error("Error fetching quiz questions:", error);
-    throw error;
-  }
+		if (response) {
+			const text = response
+				.text()
+				.replace("```json\n", "")
+				.replace("\n```", "");
+			// console.log(text);
+			return text;
+		} else {
+			throw new Error("Empty response received from Google Generative AI");
+		}
+	} catch (error) {
+		console.error("Error fetching quiz questions:", error);
+		throw error;
+	}
 }
-
-export default useGeminiApiQuestion;
--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,137 +1,137 @@
 import { useEffect, useState } from "react";
 import { QuestionCard } from "../../components/QuestionCard";
 import styles from "./quiz.module.css";
-import useGeminiApiQuestion from "../../components/utils/api";
+import { UseGeminiApiQuestion } from "../../components/utils/api";
 
 interface QuizQuestion {
-  question: string;
-  answer: string;
-  alternatives: string[];
+	question: string;
+	answer: string;
+	alternatives: string[];
 }
 
 export const Quiz = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [correctAnswers, setCorrectAnswers] = useState(0);
-  const questions = [
-    {
-      question: "Como se diz Macaco 🐒 em inglês?",
-      answers: [
-        { value: "A", label: "Monkey" },
-        { value: "B", label: "Kid" },
-        { value: "C", label: "Fancy" },
-        { value: "D", label: "Frog" },
-      ],
-      correctAnswer: "A",
-    },
-    {
-      question: "Qual é a capital do Brasil?",
-      answers: [
-        { value: "A", label: "Rio de Janeiro" },
-        { value: "B", label: "São Paulo" },
-        { value: "C", label: "Brasília" },
-        { value: "D", label: "Belo Horizonte" },
-      ],
-      correctAnswer: "C",
-    },
-    {
-      question: "Quem escreveu 'Romeu e Julieta'?",
-      answers: [
-        { value: "A", label: "William Shakespeare" },
-        { value: "B", label: "Charles Dickens" },
-        { value: "C", label: "Jane Austen" },
-        { value: "D", label: "Mark Twain" },
-      ],
-      correctAnswer: "A",
-    },
-    {
-      question: "Qual é o maior planeta do sistema solar?",
-      answers: [
-        { value: "A", label: "Marte" },
-        { value: "B", label: "Júpiter" },
-        { value: "C", label: "Vênus" },
-        { value: "D", label: "Saturno" },
-      ],
-      correctAnswer: "B",
-    },
-  ];
-  const [quizQuestions, setQuizQuestions] = useState<QuizQuestion[]>([]);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [correctAnswers, setCorrectAnswers] = useState(0);
+	const questions = [
+		{
+			question: "Como se diz Macaco 🐒 em inglês?",
+			answers: [
+				{ value: "A", label: "Monkey" },
+				{ value: "B", label: "Kid" },
+				{ value: "C", label: "Fancy" },
+				{ value: "D", label: "Frog" },
+			],
+			correctAnswer: "A",
+		},
+		{
+			question: "Qual é a capital do Brasil?",
+			answers: [
+				{ value: "A", label: "Rio de Janeiro" },
+				{ value: "B", label: "São Paulo" },
+				{ value: "C", label: "Brasília" },
+				{ value: "D", label: "Belo Horizonte" },
+			],
+			correctAnswer: "C",
+		},
+		{
+			question: "Quem escreveu 'Romeu e Julieta'?",
+			answers: [
+				{ value: "A", label: "William Shakespeare" },
+				{ value: "B", label: "Charles Dickens" },
+				{ value: "C", label: "Jane Austen" },
+				{ value: "D", label: "Mark Twain" },
+			],
+			correctAnswer: "A",
+		},
+		{
+			question: "Qual é o maior planeta do sistema solar?",
+			answers: [
+				{ value: "A", label: "Marte" },
+				{ value: "B", label: "Júpiter" },
+				{ value: "C", label: "Vênus" },
+				{ value: "D", label: "Saturno" },
+			],
+			correctAnswer: "B",
+		},
+	];
+	const [quizQuestions, setQuizQuestions] = useState<QuizQuestion[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await useGeminiApiQuestion();
-        if (response) {
-          setQuizQuestions(response);
-        } else {
-          console.error('sem resposta da api');
-        }
-      } catch (error) {
-        console.error('Error fetching', error);
-      }
-    };
-    fetchData();
-  }, []);
-  
-  console.log(quizQuestions)
-  //console.log('........................................Separador..................................')
-  //console.log(quizQuestions[0].question)
-  //console.log(quizQuestions.map(question => question.question));
-  const handleAnswer = (result: "correct" | "incorrect") => {
-    if (result === "correct") {
-      setCorrectAnswers((previous) => previous + 1);
-    }
-    setCurrentQuestion((previous) => previous + 1);
-  };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await UseGeminiApiQuestion();
+				const data = JSON.parse(response);
+				if (response) {
+					setQuizQuestions(data);
+				} else {
+					console.error("sem resposta da api");
+				}
+			} catch (error) {
+				console.error("Error fetching", error);
+			}
+		};
+		fetchData();
+	}, []);
 
-  const handleFeedback = () => {
-    const result = correctAnswers - questions.length / 2;
+	console.log(quizQuestions);
+	//console.log('........................................Separador..................................')
+	//console.log(quizQuestions[0].question)
+	//console.log(quizQuestions.map(question => question.question));
+	const handleAnswer = (result: "correct" | "incorrect") => {
+		if (result === "correct") {
+			setCorrectAnswers((previous) => previous + 1);
+		}
+		setCurrentQuestion((previous) => previous + 1);
+	};
 
-    if (result > 0)
-      return (
-        <span className={`${styles.feedback} ${styles.good}`}>Parabéns!!!</span>
-      );
-    if (result === 0)
-      return (
-        <span className={`${styles.feedback} ${styles.medium}`}>
-          Tente praticar mais
-        </span>
-      );
-    return (
-      <span className={`${styles.feedback} ${styles.bad}`}>
-        Não Desista ainda!
-      </span>
-    );
-  };
-  
+	const handleFeedback = () => {
+		const result = correctAnswers - questions.length / 2;
 
-  return (
-    <div className={styles.container}>
-      {currentQuestion < questions.length ? (
-        <QuestionCard
-          question={questions[currentQuestion].question}
-          answers={questions[currentQuestion].answers}
-          correctAnswer={questions[currentQuestion].correctAnswer}
-          onAnswer={(result: "correct" | "incorrect") => handleAnswer(result)}
-        />
-      ) : (
-        <div className={styles["feedback-container"]}>
-          <span className={styles.hits}>acertos:{correctAnswers} </span>
-          <span className={styles.errors}>
-            erros:{questions.length - correctAnswers}{" "}
-          </span>
-          {handleFeedback()}
+		if (result > 0)
+			return (
+				<span className={`${styles.feedback} ${styles.good}`}>Parabéns!!!</span>
+			);
+		if (result === 0)
+			return (
+				<span className={`${styles.feedback} ${styles.medium}`}>
+					Tente praticar mais
+				</span>
+			);
+		return (
+			<span className={`${styles.feedback} ${styles.bad}`}>
+				Não Desista ainda!
+			</span>
+		);
+	};
 
-          <button
-            onClick={() => {
-              setCurrentQuestion(0);
-              setCorrectAnswers(0);
-            }}
-            className={styles["restart-btn"]}
-          >
-            Recomeçar
-          </button>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			{currentQuestion < questions.length ? (
+				<QuestionCard
+					question={questions[currentQuestion].question}
+					answers={questions[currentQuestion].answers}
+					correctAnswer={questions[currentQuestion].correctAnswer}
+					onAnswer={(result: "correct" | "incorrect") => handleAnswer(result)}
+				/>
+			) : (
+				<div className={styles["feedback-container"]}>
+					<span className={styles.hits}>acertos:{correctAnswers} </span>
+					<span className={styles.errors}>
+						erros:{questions.length - correctAnswers}{" "}
+					</span>
+					{handleFeedback()}
+
+					<button
+						onClick={() => {
+							setCurrentQuestion(0);
+							setCorrectAnswers(0);
+						}}
+						className={styles["restart-btn"]}
+					>
+						Recomeçar
+					</button>
+				</div>
+			)}
+		</div>
+	);
 };
