2f7acaddb7c7b1f6b234a4f7e676510e01518f9f
Richard <richard.esclima@gmail.com>
2024-06-02 17:47:24 -0300

    feat: add more translation options

--- src/pages/Home/components/Header/index.tsx
+++ src/pages/Home/components/Header/index.tsx
@@ -1,13 +1,42 @@
-import { Button } from "../../../../components/Button";
 import styles from "./header.module.css";
+import {
+  getLocalStorageValue,
+  setLocalStorageValue,
+} from "../../../../components/utils/localstorage";
 
 export function Header() {
-	return (
-		<header className={styles.container}>
-			<h1>FoxTeacher</h1>
-			<div className={styles.boxButton}>
-				<Button text="Fazer Login" width={320} />
-			</div>
-		</header>
-	);
+  const options = [
+    "espanhol",
+    "ingles",
+    "italiano",
+    "japones",
+    "mandarim",
+    "portugues",
+  ];
+  const defaultValue = getLocalStorageValue<string>("originLanguage") || "";
+
+  return (
+    <header className={styles.container}>
+      <h1>FoxTeacher</h1>
+      <div className={styles.boxButton}>
+        <div className={styles["custom-select"]}>
+          <select
+            name="origin"
+            id="origin"
+            onChange={(e) => {
+              setLocalStorageValue("originLanguage", e.target.value);
+              window.location.reload();
+            }}
+            defaultValue={defaultValue}
+          >
+            {options.map((option) => (
+              <option key={`objective-${option}`} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/translations.ts
+++ src/translations.ts
@@ -125,4 +125,255 @@ export const translations = {
       },
     },
   },
+  espanhol: {
+    home: {
+      heroSection: {
+        description: "Aprenda inglés divirtiéndose",
+        getStartButton: "Comencemos",
+        AlreadyHasAccount: "Ya tengo una cuenta",
+      },
+      descriptionSection: {
+        infoText: [
+          "Nuestra Plataforma utiliza",
+          "IA",
+          "para adaptar cada lección a su ritmo y necesidades",
+        ],
+
+        trainings: {
+          title: "Nuestros Entrenamientos",
+          boxTrainings: [
+            {
+              title: "Quiz",
+              subtitle:
+                "Elija la traducción correcta para una palabra específica",
+              action: "Jugar",
+            },
+            {
+              title: "Rompecabezas",
+              subtitle:
+                "Combine palabras en inglés con sus traducciones para resolver el desafío.",
+              action: "Jugar",
+            },
+            {
+              title: "Conversación",
+              subtitle:
+                "Sumérjase en contextos cotidianos y escriba respuestas válidas para cada situación.",
+              action: "Jugar",
+            },
+          ],
+        },
+      },
+    },
+    memoryGame: {
+      mainSection: {
+        title: "Juego de Memoria",
+        cardsSelect: "Elige una cantidad",
+        idiomSelect: "Elige un idioma",
+        startButton: "Comenzar",
+      },
+    },
+    quiz: {
+      menu: {
+        selectObjectiveIdiom: "Elija el idioma que está aprendiendo",
+        numberOfAnswers: "Número de preguntas",
+        nextButton: "Siguiente",
+      },
+      feedback: {
+        good: "¡Felicidades!",
+        bad: "¡No te rindas todavía!",
+        medium: "Intenta practicar más",
+        hits: "aciertos:",
+        errors: "errores:",
+        restart: "Reiniciar",
+        newQuestions: "Nuevas Preguntas",
+      },
+    },
+  },
+  japones: {
+    home: {
+      heroSection: {
+        description: "楽しみながら英語を学びます",
+        getStartButton: "はじめましょう",
+        AlreadyHasAccount: "すでにアカウントをお持ちですか",
+      },
+      descriptionSection: {
+        infoText: [
+          "当社のプラットフォームは",
+          "AI",
+          "を使用して、各レッスンをあなたのペースやニーズに合わせて適応させます",
+        ],
+
+        trainings: {
+          title: "トレーニング",
+          boxTrainings: [
+            {
+              title: "クイズ",
+              subtitle: "特定の単語の正しい翻訳を選択してください",
+              action: "プレイ",
+            },
+            {
+              title: "パズル",
+              subtitle: "英単語とその翻訳を組み合わせて、課題を解決します。",
+              action: "プレイ",
+            },
+            {
+              title: "会話",
+              subtitle:
+                "日常の文脈に没入し、各状況に対して有効な回答を書きます。",
+              action: "プレイ",
+            },
+          ],
+        },
+      },
+    },
+    memoryGame: {
+      mainSection: {
+        title: "記憶ゲーム",
+        cardsSelect: "数量を選択してください",
+        idiomSelect: "言語を選択してください",
+        startButton: "スタート",
+      },
+    },
+    quiz: {
+      menu: {
+        selectObjectiveIdiom: "学習している言語を選択してください",
+        numberOfAnswers: "質問の数",
+        nextButton: "次へ",
+      },
+      feedback: {
+        good: "おめでとうございます！",
+        bad: "まだ諦めないで！",
+        medium: "もっと練習してみてください",
+        hits: "正解数：",
+        errors: "エラー数：",
+        restart: "リスタート",
+        newQuestions: "新しい質問",
+      },
+    },
+  },
+  mandarim: {
+    home: {
+      heroSection: {
+        description: "玩得开心学英语",
+        getStartButton: "开始学习",
+        AlreadyHasAccount: "已经有账号了",
+      },
+      descriptionSection: {
+        infoText: [
+          "我们的平台使用",
+          "AI",
+          "根据您的节奏和需求调整每节课的内容",
+        ],
+
+        trainings: {
+          title: "我们的训练",
+          boxTrainings: [
+            {
+              title: "测验",
+              subtitle: "为特定单词选择正确的翻译",
+              action: "玩",
+            },
+            {
+              title: "拼图",
+              subtitle: "将英文单词与其翻译相匹配，以解决挑战。",
+              action: "玩",
+            },
+            {
+              title: "会话",
+              subtitle: "沉浸在日常环境中，并为每种情况编写有效的回答。",
+              action: "玩",
+            },
+          ],
+        },
+      },
+    },
+    memoryGame: {
+      mainSection: {
+        title: "记忆游戏",
+        cardsSelect: "选择数量",
+        idiomSelect: "选择一种语言",
+        startButton: "开始",
+      },
+    },
+    quiz: {
+      menu: {
+        selectObjectiveIdiom: "选择您正在学习的语言",
+        numberOfAnswers: "问题数量",
+        nextButton: "下一个",
+      },
+      feedback: {
+        good: "恭喜！",
+        bad: "别放弃！",
+        medium: "试着多练习",
+        hits: "正确数：",
+        errors: "错误数：",
+        restart: "重新开始",
+        newQuestions: "新问题",
+      },
+    },
+  },
+  italiano: {
+    home: {
+      heroSection: {
+        description: "Impara l'inglese divertendoti",
+        getStartButton: "Iniziamo",
+        AlreadyHasAccount: "Ho già un account",
+      },
+      descriptionSection: {
+        infoText: [
+          "La nostra Piattaforma utilizza",
+          "IA",
+          "per adattare ogni lezione al tuo ritmo e alle tue esigenze",
+        ],
+
+        trainings: {
+          title: "I Nostri Allenamenti",
+          boxTrainings: [
+            {
+              title: "Quiz",
+              subtitle:
+                "Scegli la traduzione corretta per una parola specifica",
+              action: "Gioca",
+            },
+            {
+              title: "Puzzle",
+              subtitle:
+                "Abbinare le parole in inglese alle loro traduzioni per risolvere la sfida.",
+              action: "Gioca",
+            },
+            {
+              title: "Conversazione",
+              subtitle:
+                "Immergiti in contesti quotidiani e scrivi risposte valide per ogni situazione.",
+              action: "Gioca",
+            },
+          ],
+        },
+      },
+    },
+    memoryGame: {
+      mainSection: {
+        title: "Gioco della Memoria",
+        cardsSelect: "Scegli una quantità",
+        idiomSelect: "Scegli una lingua",
+        startButton: "Inizia",
+      },
+    },
+    quiz: {
+      menu: {
+        selectObjectiveIdiom: "Scegli la lingua che stai imparando",
+        numberOfAnswers: "Numero di domande",
+        nextButton: "Successivo",
+      },
+      feedback: {
+        good: "Congratulazioni!",
+        bad: "Non arrenderti ancora!",
+        medium: "Prova a praticare di più",
+        hits: "indovini:",
+        errors: "errori:",
+        restart: "Ricomincia",
+        newQuestions: "Nuove Domande",
+      },
+    },
+  },
 };
