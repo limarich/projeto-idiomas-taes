3df482b9ebc454d1aae803ebd9356ca154206736
Bruno Matheus Pereira Silva <82674117+bruno9800@users.noreply.github.com>
2024-06-02 14:11:13 -0300

    Links adicionados

--- src/pages/Home/components/TrainingBox/index.tsx
+++ src/pages/Home/components/TrainingBox/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../../../../components/Button";
 import styles from "./training-box.module.css";
+import { Link } from "react-router-dom";
 
 interface TrainingBoxProps {
 	title: string;
@@ -6,14 +8,16 @@ interface TrainingBoxProps {
 	bgColor: "orange" | "blue" | "purple";
 	secondary?: boolean;
 	imgSrc: string;
+	pathname: string;
 }
 
 export function TrainingBox({
 	title,
 	subtitle,
 	bgColor,
-	secondary = true,
+	secondary = false,
 	imgSrc,
+	pathname,
 }: TrainingBoxProps) {
 	const color = () => {
 		if (bgColor === "orange") return "var(--secondary)";
@@ -23,14 +27,26 @@ export function TrainingBox({
 	};
 	return (
 		<div
-			className={`${styles.container} ${secondary ?? styles.secondary}`}
+			className={`${styles.container} ${secondary ? styles.secondary : ""}`}
 			style={{
 				backgroundColor: color(),
 			}}
 		>
-			<div className={styles.content}>
-				<strong>{title}</strong>
-				<p>{subtitle}</p>
+			<div className={`${styles.content} ${secondary ? styles.secondary : ""}`}>
+				<div
+					className={`${styles.gameInfo} ${secondary ? styles.secondary : ""}`}
+				>
+					<strong>{title}</strong>
+					<p className={`${secondary ? styles.secondary : ""}`}>{subtitle}</p>
+				</div>
+				<Link
+					to={pathname}
+					style={{
+						textDecoration: "none",
+					}}
+				>
+					<Button text="Jogar" />
+				</Link>
 			</div>
 			<img src={imgSrc} alt="preview game" />
 		</div>
